ded4162e68ca9cf3d4c9765f87f581c9ef8c7a0a
B-0636 <hudongpo@youyuwo.com>
2018-07-30 11:24:42 +0800

    新增文件

--- src/main/java/nirvana/cash/loan/privilege/Application.java
+++ src/main/java/nirvana/cash/loan/privilege/Application.java
@@ -23,7 +23,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        logger.info("nirvana-cash-loan-privilege is runing ...");
+        logger.info("yofiskdk-cash-loan-privilege is runing ...");
     }
 
     @Bean
