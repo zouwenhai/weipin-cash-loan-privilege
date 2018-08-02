002a42dda2070f815afa2c8dda1108096d2de728
刘晓伟 <liuxiaowei@youyuwo.com>
2018-08-02 17:11:16 +0800

    细节优化

--- src/main/java/nirvana/cash/loan/privilege/web/filter/RequestLogZullFilter.java
+++ src/main/java/nirvana/cash/loan/privilege/web/filter/RequestLogZullFilter.java
@@ -92,8 +92,8 @@ public class RequestLogZullFilter extends ZuulFilter {
             }
             User user = (User) res.getData();
             //添加请求头参数
-            ctx.addZuulRequestHeader("loginname",user.getUsername());
-            ctx.addZuulRequestHeader("username",user.getName());
+            ctx.addZuulRequestHeader("logiName",user.getUsername());
+            ctx.addZuulRequestHeader("userName",user.getName());
 
             long endTime = System.currentTimeMillis();
 
