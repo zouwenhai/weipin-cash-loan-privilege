b4336af31e7ec39af221342966a3ac363d02b872
刘晓伟 <liuxiaowei@youyuwo.com>
2018-08-13 17:44:01 +0800

    修改部门

--- src/main/java/nirvana/cash/loan/privilege/system/controller/DeptController.java
+++ src/main/java/nirvana/cash/loan/privilege/system/controller/DeptController.java
@@ -85,9 +85,9 @@ public class DeptController extends BaseController {
 
 	//删除部门
 	@RequestMapping("dept/delete")
-	public ResResult deleteDept(Integer id) {
+	public ResResult deleteDept(String deptIds) {
 		try {
-			this.deptService.deleteDepts(id.toString());
+			this.deptService.deleteDepts(deptIds);
 			return ResResult.success();
 		} catch (Exception e) {
 			logger.error("部门管理|删除部门|执行异常:{}",e);
