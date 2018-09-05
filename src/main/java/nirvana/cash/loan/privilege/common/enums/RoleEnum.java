217a451ef0446dd8c4d97cb39423a176cf06c37c
B-0636 <hudongpo@youyuwo.com>
2018-09-05 14:13:03 +0800

    添加用户角色

--- src/main/java/nirvana/cash/loan/privilege/common/enums/RoleEnum.java
+++ src/main/java/nirvana/cash/loan/privilege/common/enums/RoleEnum.java
@@ -8,24 +8,30 @@ public enum RoleEnum {
 
     //超级管理员
     SUPERVISOR("supervisor", "超级管理员"),
-
-    //催收角色
-    COLL_MANAGER("coll_supervisor", "催收系统管理员"),
-    COLL_ADMIN("coll_admin", "催收主管", "coll"),
-    COLL_USER("coll_user", "催收专员", "coll"),
-
-    //风控角色
+    //系统管理员
+    ADMINISTRATOR("administrator", "系统管理员"),
+    //风控系统管理员
+    RISK_ADMINISTRATOR("risk_administrator", "风控系统管理员"),
+    //产品管理员
+    PRODUCT_ADMINISTRATOR("product_administrator", "产品管理员"),
+    //风控规则配置人员
+    RISK_DEVELOPER("risk_developer", "风控规则配置人员"),
+    //风控审核主管
     RISK_ADMIN("risk_admin", "风控审核主管", "risk"),
+    //风控审核专员
     RISK_USER("risk_user", "风控审核专员", "risk"),
-    RISK_DEVELOPER("risk_developer", "风控开发人员"),
-
-    //财务角色
-    FINANCIAL_ADMIN("financial_admin", "财务主管"),
+    //放款审核专员
+    LOAN_USER("loan_user", "放款审核专员"),
+    //催收系统管理员
+    COLL_MANAGER("coll_supervisor", "催收系统管理员"),
+    //催收主管(外部机构)
+    COLL_ADMIN("coll_admin", "催收主管", "coll"),
+    //催收专员(外部机构)
+    COLL_USER("coll_user", "催收专员", "coll"),
+    //财务专员
     FINANCIAL_USER("financial_user", "财务专员"),
-
-    //产品角色
-    PRODUCT_USER("product_user", "产品人员"),
-
+    //客服角色
+    CUSTOMER_USER("customer_user", "产品人员"),
     //运营角色
     OPERATION_USER("operation_user", "运营人员");
 
@@ -46,6 +52,15 @@ public enum RoleEnum {
         this.code = code;
         this.name = name;
         this.service = service;
+    }
+
+    public static RoleEnum getPaymentStatusEnumByValue(String code) {
+        for (RoleEnum roleEnum : RoleEnum.values()) {
+            if (roleEnum.getCode().equals(code)) {
+                return roleEnum;
+            }
+        }
+        return null;
     }
 
     public String getCode() {
@@ -70,15 +85,6 @@ public enum RoleEnum {
 
     public void setService(String service) {
         this.service = service;
-    }
-
-    public static RoleEnum getPaymentStatusEnumByValue(String code) {
-        for (RoleEnum roleEnum : RoleEnum.values()) {
-            if (roleEnum.getCode().equals(code)) {
-                return roleEnum;
-            }
-        }
-        return null;
     }
 
 }
