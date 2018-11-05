86d0cad6e85a3ec470c35f64c432459f6ba78709
刘晓伟 <liuxiaowei@youyuwo.com>
2018-11-05 16:04:41 +0800

    消息列表

--- src/main/java/nirvana/cash/loan/privilege/service/MsgListService.java
+++ src/main/java/nirvana/cash/loan/privilege/service/MsgListService.java
@@ -1,5 +1,6 @@
 package nirvana.cash.loan.privilege.service;
 
+import nirvana.cash.loan.privilege.common.util.ResResult;
 import nirvana.cash.loan.privilege.domain.MsgList;
 import nirvana.cash.loan.privilege.service.base.IService;
 
@@ -7,4 +8,10 @@ import nirvana.cash.loan.privilege.service.base.IService;
  * Created by Administrator on 2018/11/5.
  */
 public interface MsgListService extends IService<MsgList> {
+
+    //分页列表
+    ResResult findPageList(MsgList msgList);
+
+    //保存消息
+    ResResult saveMsg(MsgList msgList);
 }
