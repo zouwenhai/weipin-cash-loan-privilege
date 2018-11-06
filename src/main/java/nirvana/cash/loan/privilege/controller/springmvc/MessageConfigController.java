10fd4b4c6039d96853e9ef68a249578d3d19490c
B-0252 <sunyong@youyuwo.com>
2018-11-06 10:31:45 +0800

    消息管理列表添加分页

--- src/main/java/nirvana/cash/loan/privilege/controller/springmvc/MessageConfigController.java
+++ src/main/java/nirvana/cash/loan/privilege/controller/springmvc/MessageConfigController.java
@@ -31,7 +31,7 @@ public class MessageConfigController extends BaseController {
      *
      * @return
      */
-    @RequestMapping("/notauth/queryMessageConfigs")
+    @RequestMapping("/queryMessageConfigs")
     public ResResult queryMessageConfigs(QueryRequest request) {
         PageHelper.startPage(request.getPageNum(), request.getPageSize());
         List<MessageConfig> messageConfigList = messageConfigService.queryMessageConfigs();
