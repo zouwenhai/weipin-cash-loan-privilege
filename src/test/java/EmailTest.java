6c9ae1d221449c737acec292eb9066987042b8fa
刘晓伟 <liuxiaowei@youyuwo.com>
2018-11-08 18:28:00 +0800

    消息中心实现

--- src/test/java/EmailTest.java
+++ src/test/java/EmailTest.java
@@ -1,9 +1,15 @@
+import nirvana.cash.loan.privilege.common.enums.MsgModuleEnum;
+import nirvana.cash.loan.privilege.common.enums.OrderStatusEnum;
+import nirvana.cash.loan.privilege.common.util.DateUtil;
 import nirvana.cash.loan.privilege.common.util.EmaiUtil;
+import nirvana.cash.loan.privilege.common.util.FreemarkerUtil;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2018/11/7.
@@ -12,6 +18,8 @@ public class EmailTest extends BaseTest {
 
     @Autowired
     private EmaiUtil emaiUtil;
+    @Autowired
+    FreemarkerUtil freemarkerUtil;
 
     @Test
     public void sendEmail() {
@@ -22,6 +30,26 @@ public class EmailTest extends BaseTest {
         String title = "消息中心|邮件发送测试1";
         String emailContent = "测试邮件，合作愉快！";
         emaiUtil.sendEmail(fromAddress, toAddresList, title, emailContent);
+        System.err.println("done");
+    }
+
+    @Test
+    public void sendEmailHtml(){
+        String fromAddress = "liuxiaowei@yofish.com";
+        List<String> toAddresList = new ArrayList<>();
+        toAddresList.add("liuxiaowei@yofish.com");
+        //邮件标题
+        String title = MsgModuleEnum.CHECK_COLL.getName()+"模块-有新订单需要您处理";
+        //邮件内容
+        Map<String,String> msgmap=new HashMap();
+        msgmap.put("msgModule", MsgModuleEnum.CHECK_COLL.getName());
+        msgmap.put("orderId","20180929000015");
+        msgmap.put("orderStatus", OrderStatusEnum.LoanFailed.getDesc());
+        msgmap.put("time", DateUtil.getDateTime());
+        msgmap.put("userName", "刘晓伟");
+        String emailContent = freemarkerUtil.resolve("email_notice_msg.ftl",msgmap);
+        //发送邮件
+        emaiUtil.sendEmailHtml(fromAddress, toAddresList, title, emailContent);
         System.err.println("done");
     }
 }
