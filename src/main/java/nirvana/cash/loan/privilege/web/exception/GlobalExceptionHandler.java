57e351fbb60092759e50cc8c1aa1c929d8098db9
刘晓伟 <liuxiaowei@youyuwo.com>
2018-11-09 13:39:48 +0800

    系统过滤器修改

--- src/main/java/nirvana/cash/loan/privilege/web/exception/GlobalExceptionHandler.java
+++ src/main/java/nirvana/cash/loan/privilege/web/exception/GlobalExceptionHandler.java
@@ -1,13 +1,15 @@
 package nirvana.cash.loan.privilege.web.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import nirvana.cash.loan.privilege.common.util.ResResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
@@ -18,7 +20,15 @@ public class GlobalExceptionHandler {
     @ResponseBody
     public ResResult jsonErrorHandler(Exception e) {
         logger.error("[exception]:message==>{} e==>{}", e.getMessage(), e);
-        return ResResult.error(error,ResResult.ERROR);
+        return ResResult.error(error, ResResult.ERROR);
+    }
+
+    @ExceptionHandler(value = BizException.class)
+    @ResponseBody
+    public ResResult jsonErrorHandler(ServerHttpRequest request, BizException e) {
+        log.error("[exception]:request==>{},message==>{},e==>{}",
+                request.getURI(), e.getMessage(), e);
+        return ResResult.error(e.getDesc(), e.getCode());
     }
 
 
