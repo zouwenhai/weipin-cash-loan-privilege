509749ccb8704cdf04b963049992a63f8c7590af
胡冬冬 <hudongdong@youyuwo.com>
2018-12-19 09:19:32 +0800

    判断用户是否有订单所在产品的权限，没有则不接收消息

--- src/main/java/nirvana/cash/loan/privilege/mq/message/MessageFilter.java
+++ src/main/java/nirvana/cash/loan/privilege/mq/message/MessageFilter.java
@@ -1,17 +1,22 @@
 package nirvana.cash.loan.privilege.mq.message;
 
+import lombok.extern.slf4j.Slf4j;
+import nirvana.cash.loan.privilege.domain.UserWithRole;
 import nirvana.cash.loan.privilege.mq.facade.MessageFacade;
+import nirvana.cash.loan.privilege.service.DeptProductService;
 import nirvana.cash.loan.privilege.service.DeptService;
 import nirvana.cash.loan.privilege.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
-import java.util.List;
+import java.util.Objects;
 
 /**
  * @author dongdong
  * @date 2018/12/18
  */
+@Slf4j
 @Component
 public class MessageFilter {
 
@@ -19,6 +24,8 @@ public class MessageFilter {
     private DeptService deptService;
     @Autowired
     private UserService userService;
+    @Autowired
+    private DeptProductService deptProductService;
 
     /**
      * 判断用户是否有权限接收该消息
@@ -33,12 +40,24 @@ public class MessageFilter {
             return false;
         }
         //获取用户所有的部门，得到部门下的管理的产品的集合
-        //TODO
-        
-
-        List<Long> productIds = null;
-        //return productIds.contains(productId);
-        return true;
+        UserWithRole user = userService.findById(userId);
+        if (user == null) {
+            return false;
+        }
+        Long deptId = user.getDeptId();
+        if (deptId != null) {
+            String productNos = deptProductService.findProductNosByDeptId(deptId);
+            if (!StringUtils.hasText(productNos)) {
+                return false;
+            }
+            String productIdStr = String.valueOf(productId), regex = ",";
+            for (String productNo : productNos.split(regex)) {
+                if (Objects.equals(productNo, productIdStr)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 }
