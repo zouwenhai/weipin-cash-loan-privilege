e37c988325090831b62a4d12c4748bf38f61650f
liuxiaowei@youyuwo.com <Cy123098>
2018-12-20 13:05:26 +0800

    新需求-部门关联产品权限相关

--- src/main/java/nirvana/cash/loan/privilege/web/gatewayhystrix/GatewayHystrixCommandController.java
+++ src/main/java/nirvana/cash/loan/privilege/web/gatewayhystrix/GatewayHystrixCommandController.java
@@ -1,7 +1,9 @@
 package nirvana.cash.loan.privilege.web.gatewayhystrix;
 
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import nirvana.cash.loan.privilege.common.util.ResResult;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class GatewayHystrixCommandController {
 
     @RequestMapping("/notauth/gateway/hystrixTimeout")
-    public ResResult hystrixTimeout() {
+    public ResResult hystrixTimeout(ServerHttpRequest request) {
+        log.info("RemoteAddress={}",request.getRemoteAddress());
+        log.info("URI={}",request.getURI());
+        log.info("Path={}",request.getPath());
+        log.info("Headers={}", JSON.toJSONString(request.getHeaders()));
+        log.info("QueryParams={}",JSON.toJSONString(request.getQueryParams()));
         log.error("权限管理路由转发系统|hystrixTimeout,请求超时!");
         return ResResult.error("请求超时!");
     }
