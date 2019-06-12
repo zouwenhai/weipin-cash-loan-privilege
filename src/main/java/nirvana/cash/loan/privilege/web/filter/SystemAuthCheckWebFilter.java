9ce028c525ad6d3d18df49af589f1a60f39167bd
jinYunGang <jinyungang@youyuwo.com>
2019-06-12 18:11:40 +0800

    日志保存

--- src/main/java/nirvana/cash/loan/privilege/web/filter/SystemAuthCheckWebFilter.java
+++ src/main/java/nirvana/cash/loan/privilege/web/filter/SystemAuthCheckWebFilter.java
@@ -111,7 +111,9 @@ public class SystemAuthCheckWebFilter implements WebFilter {
                 TbYofishdkOptionLogDto logDto = new TbYofishdkOptionLogDto();
                 logDto.setOptionUrl(uri.toString());
                 logDto.setCreateTime(new Date());
-                logDto.setParams(request.getQueryParams().toString());
+                logDto.setParams(JSONObject.toJSONString(request.getQueryParams()));
+                log.info("保存请求参数1 params={}", request.getQueryParams().toString());
+                log.info("保存请求参数2 params={}", JSONObject.toJSONString(request.getQueryParams()));
                 if (StringUtils.isEmpty(user.getName())) {
                     logDto.setUsername(URLUtil.decode("用户名未获取到", "utf-8"));
                 } else {
